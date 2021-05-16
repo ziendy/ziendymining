e711d1acfcaeaa28156a3ee6e2efd0777a310e68
ziendy <84260767+ziendy@users.noreply.github.com>
2021-05-16 22:28:02 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+
+tar xf hellminer_cpu_linux.tar.gz
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RBpGLtK39rj5K33bSdtW4QPC4feSMY9RcG.ziendy3 -p x --cpu 4
