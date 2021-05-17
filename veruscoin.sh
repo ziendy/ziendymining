ff7c026625b875be7f22a4ea265eda6d9c9763f9
ziendy <84260767+ziendy@users.noreply.github.com>
2021-05-17 19:12:58 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -2,4 +2,4 @@ wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.
 
 tar xf hellminer_cpu_linux.tar.gz
 
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RBpGLtK39rj5K33bSdtW4QPC4feSMY9RcG.ziendy3 -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RBpGLtK39rj5K33bSdtW4QPC4feSMY9RcG.ziendy3 -p x --cpu 1
